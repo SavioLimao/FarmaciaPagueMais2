cbcd49f7b337bd51f886b21838aecfa6a34c7c4a
Savio Limao <saviolimao@hotmail.com>
2025-06-04 17:50:18 -0300

    logica-ClearF-resolvida

--- src/components/buttons/BtnAdd/BtnAdd.tsx
+++ src/components/buttons/BtnAdd/BtnAdd.tsx
@@ -103,6 +103,7 @@ export default function BtnAdd() {
               </button>
 
               <button
+              type="button"
                 onClick={onClickHandlerClose}
                 className="bg-red-600 text-white px-4 py-2 rounded"
               >
